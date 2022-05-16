b35a0559d22ecb10b07c65a18fe7918e74cd289b
katy01101011 <katy.91560@gmail.com>
2022-05-16 09:29:57 +0200

    non funziona :(

--- js/script.js
+++ js/script.js
@@ -5,14 +5,35 @@
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
+// Creare un array di 10 indirizzi mail
+
 const app = new Vue(
     {
         el: '#root',
         data: {
-
+            email: this.generateMail()
         },
-        methods: {
+        
+        created() {
+            for (let i = 0; i < 10; i++) {
+                const ulList = document.querySelector('ul');
+                const newLi = document.createElement('li');
+                this.email = this.email;
+                newLi.innerHTML = this.email;
+                ulList.append(newLi)
+                
+                
+            }
+        },
 
+        methods: {
+            generateMail() {
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then( (resp) => {
+                    this.email = resp.data.response
+                })
+            }
         }
     }
 )
